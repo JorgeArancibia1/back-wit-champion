50980ee240134cc494b8c1a149ff6115ed51c6e9
koke <jorge.arancibia.jeal@gmail.com>
2024-07-01 00:17:08 -0400

    Envs config add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('MAIN');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+  logger.log(`Server running on port ${envs.port}`);
 }
 bootstrap();
